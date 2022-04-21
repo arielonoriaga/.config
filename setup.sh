56ced58146ba7f43ef1c9642a5a9489c5f6f2773
arielonoriaga <onoriagaariel@gmail.com>
2022-04-21 12:35:00 -0300

    feat: add docker and docker-compose to setup

--- setup.sh
+++ setup.sh
@@ -4,27 +4,30 @@ sudo add-apt-repository ppa:lazygit-team/release
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io zsh gcc g++ make nodejs xclip ripgrep lazygit yarn ca-certificates curl gnupg lsb-release
 
-sudo apt-get install -y zsh gcc g++ make nodejs xclip ripgrep lazygit yarn
-
+# node
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 
-# zsh plugins
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-
 # symbolic links
-ln -sf .config/.fzf.zsh .fzf.zsh
-ln -sf .config/.p10k.zsh .p10k.zsh
-ln -sf .config/.vimrc .vimrc
-ln -sf .config/.zshrc .zshrc
+ln -sf ~/.config/.fzf.zsh .fzf.zsh
+ln -sf ~/.config/.p10k.zsh .p10k.zsh
+ln -sf ~/.config/.vimrc .vimrc
+ln -sf ~/.config/.zshrc .zshrc
 
 # lazydocker
-mkdir temporals/
-cd temporals/
+mkdir ~/temporals/
+cd ~/temporals/
 
 LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
@@ -32,20 +35,28 @@ mkdir lazydocker-temp
 
 tar xf lazydocker.tar.gz -C ~/temporals/lazydocker-temp
 
-mv ~/temporals/lazydocker-temp/lazydocker /usr/local/bin
-lazydocker --version
-
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
-mv ~/temporals/nvim.appimage /usr/bin/nvim
-
 git clone --depth 1 https://github.com/junegunn/fzf.git
-~/temporals/fzf/install --all
 
 cd ~
 
+chown ${USER}:${USER} temporals --recursive
+
+sudo mv ~/temporals/lazydocker-temp/lazydocker /usr/local/bin
+lazydocker --version
+
+sudo mv ~/temporals/nvim.appimage /usr/bin/nvim
+~/temporals/fzf/install --all
+
 rm -Rf ~/temporals/
+
+# zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 
 #reload terminal
 source ~/.zshrc
